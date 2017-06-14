5a078230208d78265027f0d3ea413c8a286c72dd
dankato <apearmor@gmail.com>
2017-06-14 14:41:03 -0700

    hobbits and cracking the code added

--- objectdrill1.js
+++ objectdrill1.js
@@ -65,3 +65,101 @@ for (var key in myArray) {
 	console.log(`${person.job} ${person.name} doesn't report to anyone`);
 	}
 }
+
+// Cracking the code (old)
+function decode(code) {
+    switch (code.charAt(0)) {
+        case 'a': return code.charAt(1);
+        case 'b': return code.charAt(2);
+        case 'c': return code.charAt(3);
+        case 'd': return code.charAt(4);
+        default: return ' ';
+    };
+}
+
+function breakup(sentance) {
+    let words = sentance.split(' ');
+    let results = [];
+    for(let i = 0; i < words.length; i++) {
+        results.push(decode(words[i])); // why in block?
+    }
+    return results.join(''); 
+}
+
+breakup('craft block argon meter bells brown croon droop');
+
+// Cracking the code (new)
+
+function decode(code) {
+	let result = [];
+   	let cipher = {
+		a: 2,
+		b: 3,
+		c: 4,
+		d: 5
+    };
+
+    let words = code.split(' ');
+	let firstLetter, desiredIndex;
+	const cipherKeys = Object.keys(cipher);
+	for (let i=0; i<words.length; i++) {
+		firstLetter = words[i][0];
+		if(cipherKeys.includes(firstLetter)) {
+		// 	if(words.indexOf(firstletter) > -1) { // old school way	
+			desiredIndex = cipher[firstLetter]-1;
+			result.push(words[i][desiredIndex]);
+		} else {
+			result.push(' ');
+		}
+	}
+	return result.join('');
+
+}
+
+decode('craft block argon meter bells brown croon droop');
+
+// 7. Factory Functions with LOTR
+
+function createCharacter(name, nickname, race, origin, attack, defense) {
+	return {
+		name, nickname, race, origin, attack, defense,
+		describe() {
+      		console.log(`${name} is a ${race} from ${origin}.`);
+    	},
+
+		evaluateFight(opp) {
+			let x = 0, y = 0;
+			if (this.attack > opp.defense) {
+				console.log('hi');
+				x = this.attack - opp.defense;
+			}
+			if (this.defense < opp.attack) {
+				y = this.defense - opp.attack;
+			}
+			console.log(`Your opponent takes ${x} damage and you receive ${y} damage`);
+		}
+	}
+}
+
+// Gandalf.describe();
+
+const characters = [
+	createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+	createCharacter('Bilbo Baggins ', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+	createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+	createCharacter("Aragorn son of Arathorn", 'aragorn', 'Man', 'Dunnedain', 6, 8),
+	createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5)
+];
+
+
+characters.push(createCharacter('Arwen', 'arwen', 'Elf', 'Woodland Realm', 9, 6));
+
+characters.find(function(char){ return char.nickname === 'aragorn' }).describe()
+
+let onlyHobbits = characters.filter(function(char){return char.race === 'Hobbit'});
+
+let onlyHighAttack = characters.filter(function(char){return char.attack > 5});
+
+
+// What if you wanted to equip a weapon for each character and change how they are described? 
+// add weapon in createCharacter function as an individual string, add it as a key and add it to the describe function.
