e0afaefe7c8411e508bc799a4dc59ba38df1cfe2
dankato <apearmor@gmail.com>
2017-06-14 11:22:00 -0700

    2nd & 3rd complete

--- objectdrill1.js
+++ objectdrill1.js
@@ -1,3 +1,7 @@
+// Object Drills gist
+
+// 1. Object initializers and methods
+
 let loaf = {
 	flour: 300,
 	water: 210,
@@ -9,3 +13,25 @@ let loaf = {
 console.log(loaf.flour);
 console.log(loaf.water);
 console.log(loaf.hydration());
+
+// 2. Iterating over an object's properties
+
+const stuff = {
+	foo: 'mud',
+	bar: 'sand',
+	fum: 'car',
+	quux: 'lion',
+	spam: 'food'
+};
+
+for (let key in stuff) {
+	console.log(key, stuff[key]);
+}
+
+// 3. Arrays in objects
+
+const myObj = {meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']};
+
+console.log(myObj.meals[3]);
+
+// 4. 
